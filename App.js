863d43dade9121766b6d3c99180b666d1c572879
Niemistoo <jappe.nie@gmail.com>
2025-04-02 10:40:05 +0300

    Save and use preferred theme option

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { ThemeProvider } from './context/themeContext'
 import ThemeWrapper from './themes/ThemeWrapper'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './localization/i18n'
-import { ImagesProvider } from './context/imageContext'
 
 export default function App() {
   return (
